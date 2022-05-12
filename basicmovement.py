fd20b87cd20ed49bcb2bd178cc0250cb2e0de1b7
penguinsocks <105312963+penguinsocks@users.noreply.github.com>
2022-05-12 11:38:02 -0700

    Update basicmovement.py

--- basicmovement.py
+++ basicmovement.py
@@ -1,9 +1,34 @@
-
 from djitellopy import tello
 from time import sleep
 
 me = tello.Tello()
-me.connnect()
-print(me.get_battery())
 
+me.takeoff()
+me.move_up(140)
+sleep(.5)
+# drone will move forward 600cm
+me.move_forward(200)
+me.move_forward(200)
+me.move_forward(200)
+sleep(.5)
+# drone will turn and then move 300cm
+me.rotate_counter_clockwise(90)
+sleep(.5)
+me.move_forward(300)
+sleep(.5)
+# drone will rotate and move froward 600cm
+me.rotate_counter_clockwise(90)
+sleep(.5)
+me.move_forward(200)
+me.move_forward(200)
+me.move_forward(200)
+sleep(.5)
+# drone will turn and move forward 300cm
+me.rotate_counter_clockwise(90)
+sleep(.5)
+me.move_forward(300)
+sleep(.5)
+# drone will turn and land
+me.rotate_counter_clockwise(90)
+sleep(.5)
 me.land()
